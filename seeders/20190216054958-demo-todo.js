adecea3638ad67ce9294be568eef1feaabe8daa9
Viplove Prakash <viploveprakash@gmail.com>
2019-02-16 01:09:30 -0500

    add todo application

---
+++ seeders/20190216054958-demo-todo.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Todos', [{
+      note: 'Hello Todos',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      note: 'Another Todo',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete('Todos', null, {});
+  }
+};
